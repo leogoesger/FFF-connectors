43c67ea0a2ee58597c3ef0e44340adc5e80d9b7a
Leo Q <leoq91@gmail.com>
2018-10-05 17:48:35 -0700

    include test suit for all tests

--- tests/time_series_conversion/test_generate_matrix.py
+++ tests/time_series_conversion/test_generate_matrix.py
@@ -8,23 +8,23 @@ from tests.time_series_conversion.test_data import get_year_limits_suit, get_emp
 
 class TestGetTimeSeriesMatrix(unittest.TestCase):
 
-    # def test_get_year_limits(self):
-    #     for test in get_year_limits_suit:
-    #         conv = TimeSeriesConversion(
-    #             test["date_array"], test["data_array"], test["offset_date"])
-    #         conv.get_year_limits()
+    def test_get_year_limits(self):
+        for test in get_year_limits_suit:
+            conv = TimeSeriesConversion(
+                test["date_array"], test["data_array"], test["offset_date"])
+            conv.get_year_limits()
 
-    #         self.assertEqual(conv.start_year, test["start_year"],
-    #                          test["test_name"] + " - start year")
-    #         self.assertEqual(conv.end_year, test["end_year"],
-    #                          test["test_name"] + " - end year")
+            self.assertEqual(conv.start_year, test["start_year"],
+                             test["test_name"] + " - start year")
+            self.assertEqual(conv.end_year, test["end_year"],
+                             test["test_name"] + " - end year")
 
-    # def test_get_empty_matrix(self):
-    #     for test in get_empty_matrix_suit:
-    #         conv = TimeSeriesConversion(
-    #             test["date_array"], test["data_array"], test["offset_date"])
-    #         conv.get_empty_matrix()
-    #         self.assertEqual(conv.final_matrix, test["sol"])
+    def test_get_empty_matrix(self):
+        for test in get_empty_matrix_suit:
+            conv = TimeSeriesConversion(
+                test["date_array"], test["data_array"], test["offset_date"])
+            conv.get_empty_matrix()
+            self.assertEqual(conv.final_matrix, test["sol"])
 
     def test_get_position_index(self):
         for test in get_position_index_suit:
