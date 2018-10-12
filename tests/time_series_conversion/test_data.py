77cfbb49ee1b63cc1b8b96aaa742188b4bd3ef1d
Leo Q <leoq91@gmail.com>
2018-10-11 19:06:14 -0700

    fix test cases

--- tests/time_series_conversion/test_data.py
+++ tests/time_series_conversion/test_data.py
@@ -2,7 +2,7 @@ get_year_limits_suit = [
     {
         "test_name": "Happy path",
         "date_array": ["10/01/2001", "10/02/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2001,
         "end_year": 2001,
@@ -10,7 +10,7 @@ get_year_limits_suit = [
     {
         "test_name": "Before offset date - start",
         "date_array": ["09/01/2001", "10/02/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2000,
         "end_year": 2001,
@@ -18,7 +18,7 @@ get_year_limits_suit = [
     {
         "test_name": "Before offset date - start/end",
         "date_array": ["09/01/2001", "09/30/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2000,
         "end_year": 2000,
@@ -26,7 +26,7 @@ get_year_limits_suit = [
     {
         "test_name": "Leap year - start",
         "date_array": ["10/01/2000", "10/02/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2000,
         "end_year": 2001,
@@ -34,7 +34,7 @@ get_year_limits_suit = [
     {
         "test_name": "Leap year - end",
         "date_array": ["10/01/2001", "10/01/2004"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2001,
         "end_year": 2004,
@@ -42,7 +42,7 @@ get_year_limits_suit = [
     {
         "test_name": "On offset date - start/end",
         "date_array": ["10/01/2001", "10/01/2002"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "start_year": 2001,
         "end_year": 2002,
@@ -50,7 +50,7 @@ get_year_limits_suit = [
     {
         "test_name": "No offset date - start/end",
         "date_array": ["10/01/2001", "10/01/2002"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": None,
         "start_year": 2001,
         "end_year": 2002,
@@ -58,7 +58,7 @@ get_year_limits_suit = [
     {
         "test_name": "No offset date - start/end",
         "date_array": ["10/01/2001", "01/01/2002"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": None,
         "start_year": 2001,
         "end_year": 2002,
@@ -69,35 +69,35 @@ get_empty_matrix_suit = [
     {
         "test_name": "Happy path",
         "date_array": ["10/01/2001", "10/02/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "sol": [[None] for i in range(366)],
     },
     {
         "test_name": "Before offset - end",
         "date_array": ["10/01/2001", "09/20/2002"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "sol": [[None] for i in range(366)],
     },
     {
         "test_name": "Before offset - start/end",
         "date_array": ["09/20/2001", "09/20/2002"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "sol": [[None, None] for i in range(366)],
     },
     {
         "test_name": "Leap year/Before Offset - start",
         "date_array": ["09/30/2000", "09/20/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "sol": [[None, None] for i in range(366)],
     },
     {
         "test_name": "Leap year - start",
         "date_array": ["10/01/2000", "09/20/2001"],
-        "data_array": [],
+        "data_array": [1, 2],
         "offset_date": "10/01",
         "sol": [[None] for i in range(366)],
     },
@@ -108,7 +108,7 @@ get_position_index_suit = [
     {
         "test_name": "Happy path",
         "date_array": ["10/01/2000", "01/01/2001", "01/01/2002"],
-        "data_array": [],
+        "data_array": [1, 2, 3],
         "offset_date": "01/01",
         "dates": ["01/03/2001", "01/01/2002", "10/01/2001", "10/01/2000"],
         "sol": [{"r": 2, "c": 1}, {"r": 0, "c": 2}, {"r": 273, "c": 1}, {"r": 274, "c": 0}],
@@ -116,7 +116,7 @@ get_position_index_suit = [
     {
         "test_name": "10/01 offset Date",
         "date_array": ["10/01/2000", "01/01/2001", "01/01/2002"],
-        "data_array": [],
+        "data_array": [1, 2, 3],
         "offset_date": "10/01",
         "dates": ["01/03/2001", "01/01/2002", "10/01/2001", "10/01/2000"],
         "sol": [{"r": 94, "c": 0}, {"r": 92, "c": 1}, {"r": 0, "c": 1}, {"r": 0, "c": 0}],
